66e7408f3a698a86116d2f2c3cdf0c226abf7d91
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-04 20:00:48 +0530

    home page and Reset-Password ejs page created

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
+// For creating session
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
+// Passport for Authentication
+const passport = require("passport");
+const localStrategy = require("./config/passport-local-strategy");
+const User = require("./models/user");
+
+app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 app.use(expressLayouts);
 
 // Extract styles and scripts from sub pages int layouts page
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // Set the ejs view Engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//configure the session middleware
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      autoRemove: "native", // remove expired sessions automatically
+      ttl: 7 * 24 * 60 * 60, // set session TTL to 7 days
+    }),
+  })
+);
+
+// Initialize Passport and use local Strategy
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(localStrategy);
+
+// Serializing the User to decide which key is to be kept in the Cookies
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserializing the User from the key in the Cookies
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    return done(null, user);
+  } catch (err) {
+    console.log("Error in Finding User --> Passport");
+    return done(err);
+  }
+});
 
 // Use Router
-app.use('/', require('./routes'));
-
-
+app.use("/", require("./routes"));
 
 app.listen(PORT, (err) => {
-    if (err) {
-      console.log(`Error in running on Port :: ${PORT}`, err);
-      return;
-    }
-  
-    console.log(`Server is Running on Port :: ${PORT}`);
-  });
+  if (err) {
+    console.log(`Error in running on Port :: ${PORT}`, err);
+    return;
+  }
+
+  console.log(`Server is Running on Port :: ${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const userController = require('../controllers/usersController');
+
 
 router.get('/', userController.signIn);
 router.get('/sign-up', userController.signUp);
+router.get('/home', userController.home);
+router.post('/create', userController.create);
+router.post('/create-session',passport.authenticate('local', {failureRedirect: '/'}),  userController.createSession);
+router.get('/destroy-session', userController.destroySession);
+router.get('/reset-password', userController.resetPassword);
+router.post('/update-password', passport.authenticate('local', {failureRedirect: 'back'}), userController.updatePassword);
+
+
 
 module.exports = router;
